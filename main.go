e800748fb8fda0b79efafb33e498a4e96a699a15
Arul6851 <12304.arul@gmail.com>
2025-03-28 11:22:31 +0530

    delete a todo

--- main.go
+++ main.go
@@ -59,6 +59,18 @@ func main() {
 	})
 
 	// Delete a Todo
+	app.Delete("/:id", func(c *fiber.Ctx) error {
+		id := c.Params("id")
+		for i, todo := range todos {
+			if fmt.Sprint(todo.ID) == id {
+				todos = append(todos[:i], todos[i+1:]...)
+				return c.SendStatus(204)
+			}
+		}
+		return c.Status(404).JSON(fiber.Map{
+			"error": "Todo not found",
+		})
+	})
 
 	log.Fatal(app.Listen(":3000"))
 }
